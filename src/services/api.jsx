2b5de5ff2e495132bb76eed5f96ff21cba526b0b
David Camelo <dacaro16@gmail.com>
2025-06-24 15:52:06 -0500

    Setup Components UI

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -17,8 +17,19 @@ const handleResponse = async (response, resource) => {
     return response.json();
 };
 
-const createApiService = (resource) => {
-    const API_URL = `${API_BASE_URL}/${resource}`;
+const handleSelectOption = (item) => {
+    if (item.user) {
+        item.user = { id: item.user };
+    } else {
+        delete item.user;
+    }
+}
+
+const createApiService = (resource, isLocal) => {
+    let API_URL = `${API_BASE_URL}/${resource}`;
+    if (isLocal) {
+        API_URL = `http://api-gateway:8080/${resource}`;
+    }
 
     return {
         getAll: async () => {
@@ -26,6 +37,7 @@ const createApiService = (resource) => {
             return handleResponse(response, resource);
         },
         create: async (item) => {
+            handleSelectOption(item);
             const response = await fetch(API_URL, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json', ...getAuthHeaders() },
@@ -34,6 +46,7 @@ const createApiService = (resource) => {
             return handleResponse(response, resource);
         },
         update: async (id, item) => {
+            handleSelectOption(item);
             const response = await fetch(`${API_URL}/${id}`, {
                 method: 'PUT',
                 headers: { 'Content-Type': 'application/json', ...getAuthHeaders() },
@@ -60,7 +73,16 @@ export const authService = {
         });
         return handleResponse(response, 'login');
     },
+    refreshToken: async (token) => {
+        const response = await fetch(`${API_BASE_URL}/auth/refresh`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ refreshToken: token }),
+        });
+        const data = await handleResponse(response, 'refresh');
+        return data.accessToken;
+    },
 };
 
-export const userService = createApiService('users');
-export const productService = createApiService('products');
+export const userService  = (isLocal) => createApiService('users', isLocal);
+export const productService = (isLocal) => createApiService('products', isLocal);
